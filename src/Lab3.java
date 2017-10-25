3938cf9d3703f700e9247d7678ab3b2e81380cfa
JosephDGarza <32659326+JosephDGarza@users.noreply.github.com>
2017-10-25 16:40:01 -0400

    revised with better formating

--- src/Lab3.java
+++ src/Lab3.java
@@ -5,12 +5,13 @@ public class Lab3 {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 
-		int y;
+		
 		String cont = "y";
-
+		System.out.println("Learn your squares and cubes!");
+		
 		while (cont.equalsIgnoreCase("y")) {
 
-			System.out.println("Learn your squares and cubes!");
+			
 
 			System.out.print("Enter an integer: ");
 			while (!scnr.hasNextInt()) {
@@ -18,16 +19,20 @@ public class Lab3 {
 				scnr.next();
 			}
 			int x = scnr.nextInt();
-			System.out.println("Number       Squared      Cubed");
-			System.out.println("=======      =======      ======");
+			System.out.printf("%-10s %-10s %-10s \n", "Number", "Squared", "Cubed");
+			System.out.printf("%-10s %-10s %-10s \n", "=======", "=======", "======");
 
-			for (y = 1; y <= x; ++y) {
-				System.out.println(y + "            " + y * y + "             " + y * y * y);
+			for (int y = 1; y <= x; ++y) {
+				System.out.printf("%-10s %-10s %-10s \n", y, y*y ,y*y*y);
+
+				
 			}
 			System.out.print("Continue? (y/n): ");
+			
 			cont = scnr.next();
 
 		}
+		System.out.println("Goodbye.");
 		scnr.close();
 	}
 }
---
+++ src/test.java
@@ -0,0 +1,8 @@
+
+public class test {
+	public static void main(String[] args) {
+		int y = 5;
+		System.out.format("|%-20d|", y);
+	}
+
+}
